3ca95bcf792ea91e09d0e5ed6048a4790dde0243
Arnab Das <arnabd102@gmail.com>
2024-01-09 20:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (21.23%) - LeetSync

--- 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.java
@@ -1,5 +1,21 @@
 class Solution {
     public String toHex(int num) {
-        return Integer.toHexString(num);
+        if(num == 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        long res = num;
+        if(num<0){
+           res = (long)(Math.pow(2,32) + num);
+        }
+        while(res != 0){
+            int rem = (int)(res%16);
+            res = res/16;
+            if(rem < 10) {
+                sb.insert(0,rem);
+            } else{
+                sb.insert(0,(char)(rem + 87));
+            }
+        }
+
+        return sb.toString();
     }
 }
