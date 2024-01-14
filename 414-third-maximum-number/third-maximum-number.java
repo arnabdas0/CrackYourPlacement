1ffc99d15087e0e84b400af6e3ef3f2e27b7cb27
Arnab Das <arnabd102@gmail.com>
2024-01-14 11:43:40 +0530

    Time: 1 ms (93.17%) | Memory: 42.5 MB (98.24%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,23 +1,32 @@
 class Solution {
     public int thirdMax(int[] nums) {
-	
-		// taking long data type since array can contain Integer.MIN_VALUE
-        long max = Long.MIN_VALUE, sMax = Long.MIN_VALUE, tMax = Long.MIN_VALUE;
-        for (int i : nums) {
-            if (i > max) {
-                tMax = sMax;
-                sMax = max;
-                max = i;
-            } else if (i > sMax && i != max) {
-                tMax = sMax;
-                sMax = i;
-            } else if (i > tMax && i != sMax && i != max) {
-                tMax = i;
+        int n = nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        if(n<3){
+            return Math.max(nums[0], nums[1]);
+        }
+
+        long maxNum1 = Long.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            maxNum1 = Math.max(nums[i], maxNum1);
+        }
+
+        long maxNum2 = Long.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            if(nums[i]<maxNum1 && nums[i]>maxNum2){
+                maxNum2 = nums[i];
             }
         }
-		
-		// if thirdMax was not updated, return the first Max
-		
-        return tMax == Long.MIN_VALUE ? (int) max : (int) tMax;
+
+        long maxNum3 = Long.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            if(nums[i]<maxNum2 && nums[i]>maxNum3){
+                maxNum3 = nums[i];
+            }
+        }
+
+        return maxNum3 == Long.MIN_VALUE ? (int)maxNum1 : (int)maxNum3;
     }
 }
