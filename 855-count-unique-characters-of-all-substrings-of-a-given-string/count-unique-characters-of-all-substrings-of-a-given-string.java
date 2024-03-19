7155ccee162ea8d054387c120b9a8c15ddf3b3bb
Arnab Das <arnabd102@gmail.com>
2024-03-19 12:13:14 +0530

    Time: 36 ms (33.26%) | Memory: 47.4 MB (26.39%) - LeetSync

--- 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.java
+++ 855-count-unique-characters-of-all-substrings-of-a-given-string/count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -1,19 +1,30 @@
 class Solution {
     public int uniqueLetterString(String s) {
         int n = s.length();
-        int[] cur = new int[26];
-        int[] last = new int[26];
-        Arrays.fill(last, -1);
-        int total = 0;
-        for (int i = 0; i < n; i++) {
-            int c = s.charAt(i) - 'A';
-            total += (i - last[c]) * cur[c];
-            cur[c] = i - last[c];
-            last[c] = i;
+        List<List<Integer>> map = new ArrayList<>();
+
+        for(int i=0; i<26; i++){
+            List<Integer> a = new ArrayList<>();
+            a.add(-1);
+            map.add(a);
         }
-        for (int i = 0; i < 26; ++i) {
-            total += (n - last[i]) * cur[i];
+
+        for(int i=0; i<n; i++){
+            char c = s.charAt(i);
+            map.get(c-'A').add(i);
         }
-        return total;
+
+        for(int i=0; i<26; i++){
+            map.get(i).add(n);
+        }
+
+        int count = 0;
+        for(int i=0; i<26; i++){
+            List<Integer> idx = map.get(i);
+            for(int j=1; j<idx.size()-1; j++){
+                count += (idx.get(j)-idx.get(j-1)) * (idx.get(j+1)-idx.get(j));
+            }
+        }
+        return count;
     }
 }
