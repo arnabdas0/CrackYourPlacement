06206fae1f8c32993fe9b9e6fec0f04d1a3d53f3
Arnab Das <arnabd102@gmail.com>
2024-06-14 20:06:54 +0530

    Time: 5 ms (45.76%) | Memory: 44.7 MB (27.22%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,7 +1,5 @@
 class Solution {
     public String intToRoman(int num) {
-       HashMap<Character, Integer> map = new HashMap<>();
-
         String ch[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
          
